a7870bde9bf49fecc092d9c443aaf98c9b1b5519
An7112 <an.nguyent@gotecq.com>
2023-03-06 15:08:18 +0700

    CSS Variables

--- src/page/home/component/button-document/documentButton.tsx
+++ src/page/home/component/button-document/documentButton.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { documentModal } from '../../../../modal/modal';
 import { documentLinks } from '../../../../util';
 import './buttonDocument.css'
 
 export const ButtonDocument = () => {
     const Navigate = useNavigate();
-    const [active, setActive] = useState('all');
+    const [searchParams] = useSearchParams();
+    const [active, setActive] = useState('');
 
     const handleActive = (param: string) => {
         Navigate({
@@ -16,8 +17,13 @@ export const ButtonDocument = () => {
         setActive(param);
     }
 
+    useEffect(() => {
+        const getParam: any = searchParams.get('documents');
+        setActive(getParam);
+    }, [searchParams])
+
     return <div className='button-frame-document'>
-        {documentLinks.map((ele:documentModal) => {
+        {documentLinks.map((ele: documentModal) => {
             return <div
                 className={`button-document-page ${ele.search === active ? 'active' : ''}`}
                 onClick={() => handleActive(ele.search)}
